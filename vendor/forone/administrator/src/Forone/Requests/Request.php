55648017a7d1b1f91505ff7a96ca88bfe0538f83
Hongyi Zhang <792959574@qq.com>
2017-02-20 18:32:56 +0800

    delete vendor

--- vendor/forone/administrator/src/Forone/Requests/Request.php
+++
@@ -1,8 +0,0 @@
-<?php namespace Forone\Requests;
-
-use Illuminate\Foundation\Http\FormRequest;
-
-abstract class Request extends FormRequest
-{
-    //
-}
--- vendor/forone/administrator/src/resources/views/ueditor/ueditor.blade.php
+++
@@ -1,3 +0,0 @@
-<link rel="stylesheet" href="/vendor/forone/ueditor/themes/default/css/ueditor.css"/>
-<script src="/vendor/forone/ueditor/ueditor.config.js"></script>
-<script src="/vendor/forone/ueditor/ueditor.all.js"></script>
